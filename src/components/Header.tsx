cad0b52e5b503cc2a61c96292959c4a30ce0d9bb
Agam Prakash <prakashagam8967@gmail.com>
2026-04-03 17:07:00 +0530

    adding the counter logic for habit complition of dates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import { Button } from "./Button"
+import { format, isToday } from "date-fns"
 import { useHabits } from "../context/useHabits"
-import{isToday} from "date-fns"
+import { Button } from "./Button"
 
-export function Header() {
+type HeaderProps = {
+    visibleDates: Date[]
+    onPrev: () => void
+    onNext: () => void
+}
+
+export function Header({ visibleDates, onNext, onPrev }: HeaderProps) {
 
     const { habits } = useHabits()
 
-    const doneToday = habits.filter(h=> h.completions.some(c => isToday(c))).length
+    const doneToday = habits.filter(h =>
+        h.completions.some(c => isToday(c))).length
 
+    const dateRange = `${format(visibleDates[0], "MMM d")} - ${format(visibleDates.at(-1)!, "MMM d")}`
 
     return (
 
@@ -18,10 +26,14 @@ export function Header() {
                     {doneToday} / {habits.length} done today</span>
             </div>
             <div className="flex flex-col gap-1 items-end ">
-                <span className="text-zinc-400 text-sm">6 Aug - 12 Aug</span>
+                <span className="text-zinc-400 text-sm">{dateRange}</span>
                 <div className="flex items-center gap-3">
-                    <Button >Prev</Button>
-                    <Button >Next</Button>
+                    <Button onClick={onPrev}>Prev</Button>
+                    <Button onClick={onNext}
+                        disabled={visibleDates.some(d => isToday(d))}
+                    >
+                        Next
+                    </Button>
                 </div>
             </div>
         </header>
